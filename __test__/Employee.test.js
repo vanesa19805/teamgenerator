0b40675350ad00a0c15bc7253a7180e4726b17e6
vanesa gonzalez <gvane1919@gmail.com>
2021-03-04 22:38:05 -0500

    finished the js and test files

---
+++ __test__/Employee.test.js
@@ -0,0 +1,26 @@
+ 
+const Employee= require("../lib/Employee")
+describe ("Employee",()=>{
+    test("constructor", ()=>{  
+      const employee= new Employee("will",25,"will@gmail.com")
+      expect(employee.name).toEqual("will")
+      expect(employee.id).toEqual(25)
+      expect(employee.email).toEqual("will@gmail.com")
+    })
+   test("getName", ()=>{    
+        const employee= new Employee("will",25,"will@gmail.com")
+        expect(employee.getName()).toEqual("will")
+    })
+    test("getid", ()=>{    
+
+        const employee= new Employee("will",25,"will@gmail.com")
+        console.log(employee.getid())
+        expect(employee.getid()).toEqual(25)
+    })
+    test("getEmail", ()=>{    
+        const employee= new Employee("will",25,"will@gmail.com")
+        expect(employee.getEmail()).toEqual("will@gmail.com")
+
+    })
+
+})
